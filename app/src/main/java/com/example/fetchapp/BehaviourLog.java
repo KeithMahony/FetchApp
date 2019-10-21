dfc8211aba817f7aab8c0f03872fa1be76b559b3
keithmahony <keithmahony4466@gmail.com>
2019-10-21 09:26:03 +0100

    V0.1

--- app/src/main/java/com/example/fetchapp/BehaviourLog.java
+++ app/src/main/java/com/example/fetchapp/BehaviourLog.java
@@ -3,13 +3,11 @@ package com.example.fetchapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-
-
-public class MainActivity extends AppCompatActivity {
+public class BehaviourLog extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_behaviour_log);
     }
 }
